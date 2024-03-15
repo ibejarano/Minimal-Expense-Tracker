9912f774934c66b9f2dc8b054c2eebef0800ff66
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2024-03-15 10:59:20 -0300

    Initial Commit

---
+++ Minimal Expense Tracker/Minimal_Expense_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minimal_Expense_TrackerApp.swift
+//  Minimal Expense Tracker
+//
+//  Created by Ignacio Bejarano on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Minimal_Expense_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
