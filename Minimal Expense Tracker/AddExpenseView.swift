fbada8236680e4f4483a17a247ef3255d888970b
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2024-03-24 16:09:57 -0300

    Added expense add view

---
+++ Minimal Expense Tracker/AddExpenseView.swift
@@ -0,0 +1,18 @@
+//
+//  AddExpenseView.swift
+//  Minimal Expense Tracker
+//
+//  Created by Ignacio Bejarano on 24/3/24.
+//
+
+import SwiftUI
+
+struct AddExpenseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddExpenseView()
+}
