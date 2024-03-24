d0e7ccbc2ddff325f717267afe01a2b07f3c0ba2
Ignacio Bejarano <ignacio_bejarano@icloud.com>
2024-03-24 15:33:25 -0300

    Expense List view Expense class def

--- Minimal Expense Tracker/ContentView.swift
+++ Minimal Expense Tracker/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Expense: Identifiable {
+    let id = UUID()
+    let name: String
+    let amount: Double
+    let category: String
+    
+    static var example = Expense(name: "Bananas", amount: Double.random(in: 1...100), category: "Grocery")
+    
+    static var exampleArray : [Expense] = [
+        Expense(name: "Bananas", amount: Double.random(in: 1...100), category: "Grocery"),
+        Expense(name: "Meat with salad", amount: Double.random(in: 1...100), category: "Food"),
+        Expense(name: "Aspirines", amount: Double.random(in: 1...100), category: "Health"),
+        Expense(name: "Rent", amount: Double.random(in: 1...100), category: "House"),
+        Expense(name: "Water supply", amount: Double.random(in: 1...100), category: "House"),
+        Expense(name: "Gas", amount: Double.random(in: 1...100), category: "House")
+    ]
+}
+
+struct ExpenseListView: View {
+    @State var expenses: [Expense]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(expenses) { expense in
+                HStack {
+                    Text(expense.name)
+                    Spacer()
+                    Text(expense.category).background(.gray)
+                    Spacer()
+                    Text(expense.amount, format: .currency(code: "USD"))
+                }
+            }.onDelete(perform: { indexSet in
+                expenses.remove(atOffsets: indexSet)
+            })
         }
-        .padding()
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses: [Expense] = Expense.exampleArray
+    
+    var body: some View {
+        ExpenseListView(expenses: expenses)
     }
 }
 
